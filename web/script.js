95e046b320f7d6c2c4ddccb3be7957bb8fc83513
Oliver Barbeau <oliver.barbeau@gmail.com>
2023-04-07 10:29:56 -0500

    initial project git upload. Game logic is nearly finished. Work to be done on linking frontend and backend. Readme needs work. Frontend should probably be put into a framework, it is just js, html, css right now.

---
+++ web/script.js
@@ -0,0 +1,301 @@
+
+
+
+
+
+const keys = [..."ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
+var player_entry_text_display_element = document.getElementById("player_entry_text_display");
+var player_entry_text = "";
+var player_state = {};
+var time_left = 60 * 20; // 20 minutes
+var entry_block = false;
+
+// a dictionary of characters and their counts
+// const initialCharacterPool = {"a": 1, "b": 1, "c": 1, "d": 1, "e": 1, "f": 1, "g": 1, "h": 1, "i": 1, "j": 1, "k": 1, "l": 1, "m": 1, "n": 1, "o": 1, "p": 1, "q": 1, "r": 1, "s": 1, "t": 1, "u": 1, "v": 1, "w": 1, "x": 1, "y": 1, "z": 1, " ": 1}
+// const initialCharacterPool = {"e": 1, "f": 1, "g": 1, "h": 1, "i": 1, "j": 1, "k": 1, "l": 1, "m": 1, "n": 1, "o": 1, "p": 1, "q": 1, "r": 1, "s": 1, "t": 1, "u": 1, "v": 1, "w": 1, "x": 1, "y": 1, "z": 1, " ": 1}
+// const initialCharacterPool = { a: 0, b: 0, c: 0, d: 5, e: 1, f: 1, g: 1, h: 1, i: 1, j: 1, k: 1, l: 1, m: 1, n: 1, o: 1, p: 1, q: 1, r: 1, s: 1, t: 0, u: 0, v: 0, w: 0, x: 0, y: 0, z: 0 }
+const initialCharacterPool = { a: 100, b: 1, c: 0, d:0, e: 6, f: 2, g: 1, h: 2, i: 3, j: 0, k: 0, l: 1, m: 0, n: 2, o: 0, p: 0, q: 0, r: 2, s: 2, t: 3, u: 0, v: 0, w: 0, x: 0, y: 0, z: 0 }
+
+var character_pool = Object.assign({}, initialCharacterPool);
+
+// enabled_key_color = rgb(97, 135, 97);
+const enabled_key_color = "#F5E28B";
+const disabled_key_color = "#736E5D";
+
+
+
+//       <li class="ring" id="X">
+/* <div class="letter_count">0</div>
+<div class="keyboard_letter">X</div>
+</li>
+<li class="middle" id="C">
+<div class="letter_count">0</div>
+<div class="keyboard_letter">C</div>
+</li>
+<li class="pointer1st" id="V">
+<div class="letter_count">0</div>
+<div class="keyboard_letter">V</div>
+</li> */
+
+
+
+
+function update_key_count(key) {
+  document.getElementById(key).getElementsByClassName("letter_count")[0].innerHTML = character_pool[key.toLowerCase()];
+}
+
+function reset_key_counts() {
+  for (const key in character_pool) {
+    update_key_count(key.toUpperCase());
+  }
+}
+
+
+function apply_key_color(key) {
+  if (character_pool[key.toLowerCase()] > 0) {
+    document.getElementById(key).classList.add("enabled");
+    document.getElementById(key).style.backgroundColor = enabled_key_color;
+    document.getElementById(key).style.border = "2px solid #A89F77";
+    // change the key color to grey
+  } else {  
+    document.getElementById(key).classList.remove("enabled");
+    document.getElementById(key).style.backgroundColor = disabled_key_color;
+    document.getElementById(key).style.border = "2px solid #968F74";
+
+  }
+}
+
+function reset_key_colors() {
+  for (const key in character_pool) {
+    apply_key_color(key.toUpperCase());
+  }
+}
+reset_key_colors();
+
+function applyKey(key) {
+  console.log(key);
+  switch (key) {
+    case "Delete":
+      if (player_entry_text.length < 1) {
+        return;
+      }
+      deleted_character = player_entry_text[player_entry_text.length - 1];
+      if (deleted_character != " ") {
+        character_pool[deleted_character.toLowerCase()] += 1;
+        if (character_pool[deleted_character] == 1) {
+          document.getElementById(deleted_character).classList.remove("disabled");
+        }
+        apply_key_color(deleted_character.toUpperCase());
+        update_key_count(deleted_character.toUpperCase());
+      }
+      player_entry_text = player_entry_text.slice(0, -1);
+
+      break;
+    
+    case "Submit":
+      // send the guess to the game server for validation
+      // retrieve the result of the guess:
+      // 1. score -500 to 1000.
+      // 2. the words that were correctly identified and their points
+      // 3. the points for the number of letters
+      // 4. the new player state:
+      //    a. the player's total score
+      //    b. the player's found words
+      //    c. the player's remaining 
+      
+      // reset the guess text entry and keyboard colors
+      player_entry_text = "";
+      character_pool = Object.assign({}, initialCharacterPool);
+      reset_key_colors();
+      reset_key_counts();
+      break;
+    
+    case "Space":
+      if (player_entry_text.length > 0 && player_entry_text[player_entry_text.length - 1] !== " ") { // don't add a space if the last character is already a space
+        player_entry_text += " ";
+      }
+      break;
+    
+    default:
+      // if the length of the player entry text is too long, lets just not add the key
+      if (player_entry_text.length >= 50) {
+        return;
+      }
+
+      // verify that the key is can be added to the player entry text
+      // are there enough characters in the character pool to add the key?
+      if (character_pool[key.toLowerCase()] > 0) {
+        // decrement the character count
+        character_pool[key.toLowerCase()] -= 1;
+        // add the key to the player entry text
+        player_entry_text += key;
+        apply_key_color(key.toUpperCase());
+        update_key_count(key.toUpperCase());
+
+      }
+      break;
+  }
+  console.log(player_entry_text);
+  document.getElementById("player_entry_text_display").innerHTML = player_entry_text.toLowerCase();
+}
+
+
+
+function start_timer() {
+  var timer = setInterval(function () {
+    var minutes = Math.floor(time_left / 60);
+    var seconds = time_left - minutes * 60;
+    seconds = seconds < 10 ? "0" + seconds : seconds;
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    document.getElementById("timer").innerHTML = minutes + ":" + seconds;
+    time_left -= 1;
+    if (time_left < 0) {
+      clearInterval(timer);
+      document.getElementById("timer").innerHTML = "0:00";
+    }
+  }, 1000);
+}
+
+
+
+document.getElementById('start-button').addEventListener('click', function() {
+  document.getElementById('start_button_container').style.display = 'none';
+  start_timer();
+});
+
+window.addEventListener('load', function () {
+  reset_key_counts();
+  reset_key_colors();
+  // user_identifier = get_user_identifier();
+  // get_game_state();
+  // get_character_pool();
+  // get_player_state();
+
+});
+
+
+const command_keys = ["Control", "Alt", "Shift", "Meta"];
+
+
+// click and keydown both apply the key but under different circumstances
+// var entry_block = false;
+
+
+
+document.addEventListener("keydown", ({ key }) => {
+  if (entry_block) {
+    return;
+  }
+  
+  console.log(key)
+  var keyElementID = null;
+  var keyElement = null;
+  switch (key) {
+    case "Backspace":
+      keyElementID = "Delete"
+      console.log("Key is Backspace")
+      break;
+    case " ":
+      console.log("Key is Space")
+      keyElementID = "Space"
+      // applyKey("Space");
+      break;
+    case "Enter":
+      keyElementID = "Submit"
+      console.log("Key is Enter")
+      break;
+    default:
+      if (key.match(/^[a-z]$/i)) {
+        keyElementID = key.toUpperCase();
+        console.log("Key is alpha")
+      }
+      else {
+        console.log("Key is not used")
+        // if it is a command key like ctrl, alt, shift, etc. then 
+        // turn on the entry block so that the keydown event doesn't
+        // apply keys until the keyup event is triggered
+
+        if (command_keys.includes(key)) {
+          entry_block = true;
+        }
+        return;
+      }
+      break;
+    }
+    // is the key alpha ? a-z, A-Z
+    
+    keyElement = document.getElementById(keyElementID);
+    
+    
+    keyElement.classList.add("hit")
+    keyElement.addEventListener('animationend', () => {
+      keyElement.classList.remove("hit")
+    })
+    applyKey(keyElementID);
+})
+
+
+// keyup event for when the user is holding down a command key, 
+// we need to wait for the keyup event to apply the key
+// the keyup will listen for command keys listed in the command_keys array
+
+document.addEventListener("keyup", ({ key }) => {
+  if (entry_block) {
+    entry_block = false;
+  }
+})
+
+
+
+
+keys.push("Delete");
+keys.push("Submit");
+// keys.push("Space");
+
+const keyElements = keys.map((key) => document.getElementById(key));
+for (const keyElement of keyElements) {
+  keyElement.addEventListener("click", () => {
+    keyElement.classList.add("hit")
+    keyElement.addEventListener('animationend', () => {
+      keyElement.classList.remove("hit")
+    })
+    applyKey(keyElement.id);
+  })
+}
+
+space_keyElement = document.getElementById("Space");
+
+space_keyElement.addEventListener("click", () => {
+  space_keyElement.classList.add("hit")
+  space_keyElement.addEventListener('animationend', () => {
+    space_keyElement.classList.remove("hit")
+  })
+  applyKey("Space");
+})
+// document.addEventListener("click", ({ key }) => {   
+//   console.log(key)
+  
+//   keyElement.classList.add("hit")
+//   keyElement.addEventListener('animationend', () => {
+//     keyElement.classList.remove("hit")
+//   })
+// })
+
+
+
+
+
+
+
+
+
+
+// targetRandomKey();
+
+  // if (keyPressed === highlightedKey.innerHTML) {
+  //   timestamps.unshift(getTimestamp());
+  //   const elapsedTime = timestamps[0] - timestamps[1];
+  //   console.log(`Character per minute ${60/elapsedTime}`)
+  //   highlightedKey.classList.remove("selected");
+  //   targetRandomKey();
+  // }
